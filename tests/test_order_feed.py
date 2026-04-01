bc0ed2f04c1461ce4773a2a25cbb71a59473bc35
mrsMistral <obalzak@list.ru>
2026-04-01 21:14:19 +0200

    подправила тесты order_feed в части прямого обращения к driver

--- tests/test_order_feed.py
+++ tests/test_order_feed.py
@@ -1,9 +1,7 @@
+import allure
 from pages.feed_page import FeedPage
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
-from locators.feed_page_locators import FeedPageLocators
-from config.urls import LOGIN, MAIN_PAGE, FEED_PAGE
-import allure
 from config.credentials import TEST_EMAIL, TEST_PASSWORD
 
 @allure.feature("Лента заказов")
@@ -11,87 +9,75 @@ class TestOrderFeed:
 
     @allure.title("Открытие модального окна заказа")
     def test_open_order_details_modal(self, driver):
-
         feed = FeedPage(driver)
-        feed.open_feed()   
-
+        feed.open_feed()
         feed.open_first_order()
 
-        modal = feed.find(FeedPageLocators.ORDER_MODAL_TITLE)
-        assert modal.is_displayed()
+        assert feed.is_order_modal_visible(), "Модальное окно заказа не отображается"
 
     @allure.title("Авторизованный пользователь видит заказы в ленте")
     def test_user_orders_visible_in_feed(self, driver):
-
-        driver.get(LOGIN)
         login = LoginPage(driver)
+        login.open()
         login.login(TEST_EMAIL, TEST_PASSWORD)
 
-        driver.get(FEED_PAGE)
         feed = FeedPage(driver)
+        feed.open_feed()
 
-        orders = feed.get_all_orders()
-        assert len(orders) > 0
+        assert feed.get_all_orders_count() > 0, "Список заказов пуст"
 
     @allure.title("Общий счётчик увеличивается после создания заказа")
     def test_total_counter_increases_after_order(self, driver):
-
         feed = FeedPage(driver)
         feed.open_feed()
-
         total_before = feed.get_total_counter()
 
-        driver.get(LOGIN)
         login = LoginPage(driver)
+        login.open()
         login.login(TEST_EMAIL, TEST_PASSWORD)
 
-        driver.get(MAIN_PAGE)
-
         main = MainPage(driver)
+        main.open()
         main.drag_ingredient_to_constructor()
         main.create_order()
 
-        driver.get(FEED_PAGE)
-
+        feed.open_feed()
         total_after = feed.get_total_counter()
-        assert total_after > total_before
+        assert total_after > total_before, "Общий счётчик не увеличился после создания заказа"
 
     @allure.title("Счётчик заказов за сегодня увеличивается после создания заказа")
     def test_today_counter_increases_after_order(self, driver):
-
         feed = FeedPage(driver)
         feed.open_feed()
-
         today_before = feed.get_today_counter()
 
-        driver.get(LOGIN)
         login = LoginPage(driver)
+        login.open()
         login.login(TEST_EMAIL, TEST_PASSWORD)
 
-        driver.get(MAIN_PAGE)
-
         main = MainPage(driver)
+        main.open()
         main.drag_ingredient_to_constructor()
         main.create_order()
 
-        driver.get(FEED_PAGE)
-
+        feed.open_feed()
         today_after = feed.get_today_counter()
-        assert today_after > today_before
+        assert today_after > today_before, "Счётчик заказов за сегодня не увеличился"
 
     @allure.title("Созданный заказ отображается в статусе 'В работе'")
-    def test_order_appears_in_progress(self, driver): 
+    def test_order_appears_in_progress(self, driver):
+        login = LoginPage(driver)
+        login.open()
+        login.login(TEST_EMAIL, TEST_PASSWORD)
 
-        driver.get(LOGIN) 
-        login = LoginPage(driver) 
-        login.login(TEST_EMAIL, TEST_PASSWORD) 
-        driver.get(MAIN_PAGE) 
-        main = MainPage(driver) 
-        main.drag_ingredient_to_constructor() 
-        main.create_order() 
-        order_number = main.get_order_number() 
-        main.close_order_modal() 
-        driver.get(FEED_PAGE) 
-        feed = FeedPage(driver) 
+        main = MainPage(driver)
+        main.open()
+        main.drag_ingredient_to_constructor()
+        main.create_order()
+        order_number = main.get_order_number()
+        main.close_order_modal()
+
+        feed = FeedPage(driver)
+        feed.open_feed()
 
         assert feed.is_order_in_progress(order_number), f"Заказ #{order_number} не найден в 'В работе'"
